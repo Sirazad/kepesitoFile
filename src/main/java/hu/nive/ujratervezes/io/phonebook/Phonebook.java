943493615fcf6c407aab607914594c46214aad0c
Sirazad <lajosimola2@gmail.com>
2021-04-29 11:59:01 +0200

    pass 4 tests

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -1,3 +1,24 @@
 package hu.nive.ujratervezes.io.phonebook;
 
-public class Phonebook {}
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Map;
+
+public class Phonebook {
+
+
+     public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
+         if (contacts == null || output == null) throw new IllegalArgumentException();
+         try( BufferedWriter writer = new BufferedWriter(new FileWriter(output))){
+             writer.write("");
+         }
+         try(BufferedWriter writer = new BufferedWriter(new FileWriter(output, true))) {
+             for (Map.Entry line :  contacts.entrySet()) {
+                 writer.append(line.getKey()+": "+line.getValue());
+             }
+         }
+     }
+
+
+}
