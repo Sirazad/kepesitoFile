37b0d9ae2ca1b46fba9ebc79b326630c26a50356
Sirazad <lajosimola2@gmail.com>
2021-04-29 12:05:51 +0200

    pass all tests

--- src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
+++ src/main/java/hu/nive/ujratervezes/io/phonebook/Phonebook.java
@@ -10,15 +10,15 @@ public class Phonebook {
 
      public void exportPhonebook(Map<String, String> contacts, String output) throws IOException {
          if (contacts == null || output == null) throw new IllegalArgumentException();
+
          try( BufferedWriter writer = new BufferedWriter(new FileWriter(output))){
              writer.write("");
          }
+
          try(BufferedWriter writer = new BufferedWriter(new FileWriter(output, true))) {
              for (Map.Entry line :  contacts.entrySet()) {
-                 writer.append(line.getKey()+": "+line.getValue());
+                 writer.append(line.getKey()+": "+line.getValue()+"\n");
              }
          }
      }
-
-
 }
